bd1dd38a439a2af659b3af4fe1e7ea4d0b005680
Andrew Chen <achen1192@gmail.com>
2021-12-05 12:49:09 -0500

    buttontest

--- audioPlay.py
+++ audioPlay.py
@@ -8,8 +8,14 @@ running = False
 while True:
     if button.is_pressed and not running:
         running = True
-        os.system("sudo aplay -D hw:2 ChugJug.wav &")
-        time.sleep(0.5)
+        print("preset")
+        song = audioFiles[random.randrange(0,len(audioFiles))]
+        audioFiles.remove(song)
+        if (song.startswith("COC")):
+            os.system("amixer -c 2 -- sset Speaker playback 6.00dB")
+            os.system("sudo aplay -D hw:2 ./AudioFiles/\"" + song + "\"")
+        else:
+            os.system("sudo aplay -D hw:2 ./AudioFiles/\"" + song + "\" &")
 
     elif button.is_pressed and running:
         running = False
