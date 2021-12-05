9d9c02308e95643ada12cbd0a9d21a4481fe875e
Andrew Chen <achen1192@gmail.com>
2021-12-05 12:11:49 -0500

    buttontest

--- buttonTest.py
+++ buttonTest.py
@@ -1,9 +1,12 @@
 from gpiozero import Button
+import os
+import time
 
 button = Button(5)
 
 while True:
     if button.is_pressed:
-        print("Button is pressed")
+        os.system("sudo aplay -D hw:2 ChugJug.wav &")
+        time.sleep(0.5)
     else:
         print("Button is not pressed")
