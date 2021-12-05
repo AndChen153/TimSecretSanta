c7a27c3bcefa3d0f02535480d61642a706ee1fa2
Andrew Chen <achen1192@gmail.com>
2021-12-05 12:53:25 -0500

    buttontest

--- audioPlay.py
+++ audioPlay.py
@@ -16,17 +16,18 @@ for file in os.listdir("./RecordedFiles/"):
     if file.endswith(".wav"):
         recordedFiles.append(file)
 
+all_Audio = audioFiles
+all_Recorded = recordedFiles
+print(all_Audio, all_Recorded)
+
 while True:
     if button.is_pressed and not running:
         running = True
-        print("preset")
         song = audioFiles[random.randrange(0,len(audioFiles))]
         audioFiles.remove(song)
         if (song.startswith("COC")):
             os.system("amixer -c 2 -- sset Speaker playback 6.00dB")
-            os.system("sudo aplay -D hw:2 ./AudioFiles/\"" + song + "\"")
-        else:
-            os.system("sudo aplay -D hw:2 ./AudioFiles/\"" + song + "\" &")
+        os.system("sudo aplay -D hw:2 ./AudioFiles/\"" + song + "\" &")
 
     elif button.is_pressed and running:
         running = False
--- manualAudioPlay.py
+++ manualAudioPlay.py
@@ -1,9 +1,12 @@
 import os
 import random
+
 volumes = ["-114.00", "-25.00", "-12.00", "-1.00", "6.00"]
 iterator = 0
 recorded_iterator = 0
 os.system("amixer -c 2 -- sset Speaker playback -5.00dB")
+
+
 audioFiles = []
 for file in os.listdir("./AudioFiles/"):
     if file.endswith(".wav"):
@@ -33,18 +36,22 @@ while (True):
             os.system("amixer -c 2 -- sset Speaker playback 6.00dB")
         os.system("sudo aplay -D hw:2 ./AudioFiles/\"" + song + "\" &")
         # print("sudo aplay -D hw:2 ./AudioFiles/" + audioFiles[random.randrange(0,len(audioFiles))])
+
     elif (selection == "2"):
         os.system("sudo aplay -D hw:2 ChugJug.wav &")
         # print("sudo aplay -D hw:2 ChugJug.wav")
+
     elif (selection == "3"):
         os.system("sudo arecord -D hw:2 -f S32_LE -r 16000 -c 2 recorded" + str(recorded_iterator) + ".wav &")
         recorded_iterator += 1
         # print("sudo arecord -D hw:2 -f S32_LE -r 16000 -c 2 recorded.wav")
+
     elif (selection == "4"):
         song = recordedFiles[random.randrange(0,len(recordedFiles))]
         recordedFiles.remove(song)
         os.system("sudo aplay -D hw:2 ./RecordedFiles/\"" + song + "\" &")
         # print("sudo aplay -D hw:2 recorded.wav")
+
     elif (selection == "5"):
         if (iterator == 4):
             iterator = 0
@@ -52,6 +59,7 @@ while (True):
             iterator += 1
         os.system("amixer -c 2 -- sset Speaker playback " + volumes[iterator] +"dB &")
         # print("amixer -c 1 -- sset Master playback " + volumes[iterator] +"dB")
+
     elif (selection == "6"):
         print("killed")
         os.system("sudo killall aplay")
