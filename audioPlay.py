f80790fdfc54cffa44f434db713f7a1b9761b075
Andrew Chen <achen1192@gmail.com>
2021-12-07 16:52:30 -0500

    cursewords

--- audioPlay.py
+++ audioPlay.py
@@ -3,19 +3,27 @@ import os
 import random
 import time
 
+
 button = Button(5)
+
 cycleVolumeButton = Button(6)
 cycleAudioTypeButton = Button(13)
 recordButton = Button(12)
+cursesButton = Button(16)
+buttonNames = ["ChugJug", "Random Audio", "Recorded Audio", "Custom Audio"]
+
 
 selection = 0
 iterator = 0
 
+
 volumes = ["-114.00", "-25.00", "-12.00", "-1.00", "6.00"]
 os.system("amixer -c 0 -- sset Speaker playback -12.00dB")
 
+
 go = True
 recordGo = True
+
 
 audioFiles = []
 for file in os.listdir("./AudioFiles/"):
@@ -26,23 +34,43 @@ recordedFiles = []
 for file in os.listdir("./RecordedFiles/"):
     if file.endswith(".wav"):
         recordedFiles.append(file)
+recorded_iterator = len(recordedFiles)
+
+
+curseFiles = []
+for file in os.listdir("./CurseFiles/"):
+    if file.endswith(".wav"):
+        curseFiles.append(file)
 
 all_Audio = audioFiles
 all_Recorded = recordedFiles
-print(all_Audio, all_Recorded)
+all_Curse = curseFiles
+# print(all_Audio, all_Recorded)
+
 
 while True:
     if len(audioFiles) < 3:
         audioFiles = all_Audio
     if len(recordedFiles) < 3:
         recordedFiles = all_Recorded
+    if len(curseFiles) < 3:
+        curseFiles = all_Curse
+
 
     if cycleAudioTypeButton.is_pressed:
         if selection > 3:
             selection = 0
         else:
             selection += 1
+        print(buttonNames[selection])
         time.sleep(0.5)
+
+
+    if cursesButton.is_pressed:
+        selection = 10
+        print("Curses Audio")
+        time.sleep(0.5)
+
 
     if cycleVolumeButton.is_pressed:
         if (iterator == 4):
@@ -52,19 +80,18 @@ while True:
         os.system("amixer -c 0 -- sset Speaker playback " + volumes[iterator] +"dB &")
         time.sleep(0.5)
 
+
     if recordButton.is_pressed and recordGo:
-        recordedFiles = []
-        for file in os.listdir("./RecordedFiles/"):
-            if file.endswith(".wav"):
-                recordedFiles.append(file)
-        # os.system("sudo arecord -D hw:0 -f S32_LE -r 16000 -c 0 ./RecordedFiles/recorded" + str(len(recordedFiles)) + ".wav &")
-        os.system("sudo python3 ./recordwav.py ./RecordedFiles/recorded" + str(len(recordedFiles)) + ".wav &")
+        os.system("sudo arecord -D hw:0 -f S32_LE -r 16000 -c 2 ./RecordedFiles/recorded" + str(recorded_iterator) + ".wav &")
+        recorded_iterator += 1
+        # os.system("sudo python3 ./recordwav.py ./RecordedFiles/recorded" + str(len(recordedFiles)) + ".wav &")
         recordGo = False
         time.sleep(0.5)
     elif recordButton.is_pressed and not recordGo:
         os.system("sudo killall python3")
         time.sleep(0.5)
         recordGo = True
+
 
     if selection == 0 and button.is_pressed and go:
         # wavFile = input("Enter a wav filename: ")
@@ -76,8 +103,11 @@ while True:
     elif selection == 1 and button.is_pressed and go:
         song = audioFiles[random.randrange(0,len(audioFiles))]
         audioFiles.remove(song)
+        if (song.startswith("COC")):
+            os.system("amixer -c 0 -- sset Speaker playback 6.00dB")
         os.system("sudo python3 ./playwav.py ./AudioFiles/\"" + song + "\" &")
         print("sudo python3 ./playwav.py ./AudioFiles/\"" + song + "\" &")
+        os.system("amixer -c 0 -- sset Speaker playback " + volumes[iterator] +"dB &")
         time.sleep(0.5)
         go = False
     elif selection == 2 and button.is_pressed and go:
@@ -90,6 +120,13 @@ while True:
     elif selection == 3 and button.is_pressed and go:
         os.system("sudo aplay -D hw:0 customRecordedAudio.wav &")
         print("sudo aplay -D hw:0 customRecordedAudio.wav &")
+        time.sleep(0.5)
+        go = False
+    elif selection == 10 and button.is_pressed and go:
+        song = curseFiles[random.randrange(0,len(curseFiles))]
+        curseFiles.remove(song)
+        os.system("sudo python3 ./playwav.py ./CurseFiles/\"" + song + "\" &")
+        print("sudo python3 ./playwav.py ./CurseFiles/\"" + song + "\" &")
         time.sleep(0.5)
         go = False
     elif button.is_pressed and not go:
