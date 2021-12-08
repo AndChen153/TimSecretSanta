28b8c2b1c8cfe9b248f02f1c99588572ff2f7a98
Andrew Chen <achen1192@gmail.com>
2021-12-08 00:47:15 -0500

    working

--- runFromBoot.py
+++ runFromBoot.py
@@ -1,3 +1,4 @@
 import os
+os.system("amixer -c 0 -- sset Speaker playback -25.00dB")
 os.system("sudo python3 /home/pi/TimSecretSanta/playwav.py /home/pi/TimSecretSanta/AudioFiles/COC.wav")
-os.system("sudo python /home/pi/TimSecretSanta/audioPlay.py")
+os.system("sudo python3 /home/pi/TimSecretSanta/audioPlay.py")
